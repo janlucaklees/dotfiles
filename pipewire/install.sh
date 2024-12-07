4a1b1be0e22e822ad858cbe3a79da59f978e1a4b
Jan-Luca Klees <email@janlucaklees.de>
2024-12-08 00:13:46 +0100

    Added install script for pipewire

---
+++ pipewire/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# Installing dependencies
+yay -S --needed --noconfirm \
+    pipewire \
+    pipewire-audio \
+    pipewire-alsa \
+    pipewire-pulse \
+    pipewire-jack \
+    wireplumber
+
+# Removing no longer needed stuff.
+yay -Rs --noconfirm pulseaudio-alsa
+
+# Start the service
+systemctl --user enable --now pipewire.service pipewire-pulse.service wireplumber.service
