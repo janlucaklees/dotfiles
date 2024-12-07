e3daaba3fd6d65d149aee2188130ec67109eed51
Jan-Luca Klees <email@janlucaklees.de>
2024-12-07 20:02:34 +0100

    Fixed Gnome config some more

--- gnome/configure.sh
+++ gnome/configure.sh
@@ -33,14 +33,14 @@ gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swapcaps', 'al
 gsettings set org.gnome.mutter workspaces-only-on-primary true
 gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 ['<Super>1']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 ['<Super>2']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 ['<Super>3']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 ['<Super>4']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 ['<Super>5']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 ['<Super>6']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 ['<Super>7']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 ['<Super>8']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 ['<Super>9']
-gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-0 ['<Super>0']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>6']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 "['<Super>7']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 "['<Super>8']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 "['<Super>9']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-0 "['<Super>0']"
 
