1bde990ed8b378e4057b113e00edc240b08522f0
Jan-Luca Klees <email@janlucaklees.de>
2024-12-07 18:35:06 +0100

    Extracted install script for yay into separate module

---
+++ yay/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+echo "Installing dependencies..."
+pacman -S --needed --noconfirm fakeroot
+
+echo "Installing yay..."
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+makepkg -si --noconfirm
+cd ..
+rm -rf yay-bin
+
+echo "Doing some after care..."
+yay -Y --gendb
+yay -Syu --devel
+yay -Y --devel --save
+
+echo "Updating mirrorlist..."
+curl -s "https://archlinux.org/mirrorlist/?country=DE&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
