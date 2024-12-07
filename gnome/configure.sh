a0fc102855b7b7ee1540a541543d896bbd22ef6f
Jan-Luca Klees <email@janlucaklees.de>
2024-12-07 23:08:33 +0100

    Configuration for keyboard layouts in gnome

--- gnome/configure.sh
+++ gnome/configure.sh
@@ -29,6 +29,7 @@ gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-autom
 gsettings set org.gnome.settings-daemon.plugins.color night-light-last-coordinates "(51.9, 8.8)"
 ## Input configuration
 gsettings set org.gnome.desktop.input-sources xkb-options "['compose:rctrl', 'lv3:ralt_switch', 'ctrl:swapcaps', 'altwin:swap_alt_win']"
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'us+altgr-intl'), ('xkb', 'de')]"
 ## Workspace configuration
 gsettings set org.gnome.mutter workspaces-only-on-primary true
 gsettings set org.gnome.mutter dynamic-workspaces false
