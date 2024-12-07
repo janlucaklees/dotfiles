1f60445fc38f7946f3ab20b5f21dbc7bae3a97e5
Jan-Luca Klees <email@janlucaklees.de>
2024-12-07 19:54:55 +0100

    Extended Gnome config

--- gnome/configure.sh
+++ gnome/configure.sh
@@ -12,6 +12,35 @@ gnome-extensions enable order-extensions@wa4557.github.com
 gnome-extensions enable Vitals@CoreCoding.com
 
 # Install and set configuration
+## Appearance
 stow -vv -d $(dirname "$0") -t $HOME config
 gsettings set org.gnome.desktop.background picture-uri file:///$HOME/.background.jpg
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer', 'autoclose-xwayland', 'variable-refresh-rate', 'xwayland-native-scaling']"
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface clock-format '24h'
+gsettings set org.gnome.desktop.interface clock-show-seconds true
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+## Night Light
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 2700
+gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-last-coordinates (51.9, 8.8)
+## Input configuration
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swapcaps', 'altwin:swap_alt_win']"
+## Workspace configuration
+gsettings set org.gnome.mutter workspaces-only-on-primary true
+gsettings set org.gnome.mutter dynamic-workspaces false
+gsettings set org.gnome.desktop.wm.preferences num-workspaces 10
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 ['<Super>1']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 ['<Super>2']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 ['<Super>3']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 ['<Super>4']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 ['<Super>5']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 ['<Super>6']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-7 ['<Super>7']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-8 ['<Super>8']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-9 ['<Super>9']
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-0 ['<Super>0']
+
