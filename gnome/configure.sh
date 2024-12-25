4125a98d1c79283e08b3838b63dcf4dd172ddcb8
Jan-Luca Klees <email@janlucaklees.de>
2024-12-25 19:38:29 +0100

    Launch foot terminal such that it is compatible with other terminals
    Otherwise sshing onto some other machine becomes really obnoxious.

--- gnome/configure.sh
+++ gnome/configure.sh
@@ -108,6 +108,6 @@ gsettings set org.gnome.shell.extensions.arcmenu arcmenu-hotkey "['<Super>space'
 ### Shortcut to launch terminal
 gsettings set org.gnome.shell.extensions.forge.keybindings window-swap-last-active "[]"
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Super>Return'
-gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'foot'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'foot --term xterm-256color'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'Launch Terminal'
 gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
