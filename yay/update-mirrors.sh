48b81885d374ffffb51328a25b5fc2b55840dce6
Jan-Luca Klees <email@janlucaklees.de>
2026-02-23 20:45:04 +0100

    Improved curl command for getting mirrorlist

--- yay/update-mirrors.sh
+++ yay/update-mirrors.sh
@@ -7,4 +7,4 @@ echo "Installing dependencies..."
 doas pacman -S --needed --noconfirm pacman-contrib
 
 echo "Updating mirrorlist..."
-curl -s "https://archlinux.org/mirrorlist/?country=DE&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - | doas tee /etc/pacman.d/mirrorlist
+curl -fsSL "https://archlinux.org/mirrorlist/?country=DE&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - | doas tee /etc/pacman.d/mirrorlist
